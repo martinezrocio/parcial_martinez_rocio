dd90dba51b02aee0d667fed11a8d6fbb5d241fd7
martinezrocio <martiroci.98@gmail.com>
2023-05-31 18:00:03 -0300

    commit

---
+++ parcial-martinez-rocio/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+      <div className='App'>
+        <h1>Carga de estudiantes</h1>
+        <form></form>
+        <Card/>
+
+        <h1></h1>
+
+      </div>
+     
+ 
+  )
+}
+
+export default App
---
+++ parcial-martinez-rocio/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+       Hola, soy una tarjeta
+
+    </div>
+  )
+}
+
+export default Card
